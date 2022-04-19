4a1d8045b1819e2832c8a266019792396c7a37c2
andrea-everett <andrea.dawn.everett@gmail.com>
2022-04-19 12:54:23 -0400

    edits

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ const textBox = document.getElementById('text-box');
 
 // Dark or Light Images
 function imageMode(color) {
-    image1.src = `img/undraw_dream_world_${color}}.svg`;
+    image1.src = `img/undraw_dream_world_${color}.svg`;
     image1.src = `img/undraw_dream_world_${color}.svg`;
     image1.src = `img/undraw_dream_world_${color}.svg`;
 }
 
-function toggleDarkLightMode() {
+function toggleDarkLightMode(isDark) {
     nav.style.backgroundColor = isDark ?  'rgb(0 0 0 / 50%)' :  'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)'  : 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
-    isDark ? toggleIcon.children[1].classList.replace ('fa-sun', 'fa-moon');
+    isDark ? toggleIcon.children[1].classList.replace ('fa-sun', 'fa-moon') :
          toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
     isDark ? imageMode('dark') : imageMode('light');
 }
 
-// Switch Theme DYnamically
+// Switch Theme Dynamically
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
